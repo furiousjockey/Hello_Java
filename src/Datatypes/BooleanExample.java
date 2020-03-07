7f5bc90fd67c6446874d456ac3b0d8f265412f67
furiousjockey <yadavshubhu5150@gmail.com>
2020-03-07 14:02:39 +0530

    local commit3

--- src/Datatypes/BooleanExample.java
+++ src/Datatypes/BooleanExample.java
@@ -2,7 +2,7 @@ package Datatypes;
 
 public class BooleanExample {
     public static void main(String[] args) {
-         boolean a = true;
-        System.out.println(a);
+         boolean b = true;
+        System.out.println(b);
     }
 }
